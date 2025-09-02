e090dc25fe3334dcd457e62626c7cc1d38c17a5c
noxie <reiwallace@yahoo.com>
2025-09-02 01:04:23 +0100

    Add test case

---
+++ 2185_count_prefix/test_2815.py
@@ -0,0 +1,10 @@
+from problem_2815_attempt_1 import Solution
+import pytest
+
+@pytest.mark.parametrize("words, pref, expected", [
+    (["pay","attention","practice","attend"], "at", 2),
+    (["leetcode","win","loops","success"], "code", 0)
+])
+
+def test_prefix_count(words, pref, expected):
+    assert Solution.prefixCount("", words, pref) == expected
