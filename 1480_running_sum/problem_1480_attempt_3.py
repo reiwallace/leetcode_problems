223abd5fb52f59053421b991def169e7da313505
noxie <reiwallace@yahoo.com>
2025-09-06 18:34:57 +0100

    Add third attempt attempt

---
+++ 1480_running_sum/problem_1480_attempt_3.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = [nums[0]]
+        for i in range(1, len(nums)):
+            running_sum.append(running_sum[i - 1] + nums[i])
+        return running_sum
