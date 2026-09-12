0f8cf9555eb130703a48b09d41fda1926530e68c
reiwallace <reiwallace@yahoo.com>
2026-09-12 12:22:42 +0100

    Upload 1342 cpp revision

---
+++ Solutions/Kata Questions/1342_steps_to_reach_zero/problem_1342_cpp_1.py
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+        for(steps = 0; num > 0; steps++) {
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+        }
+        return total;
+    }
+};
